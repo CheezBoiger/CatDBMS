048a25d3b691e21efaa2db9d1bbba3fe21eac97f
CheezBoiger <alexander.garcia18@yahoo.com>
2015-12-11 19:35:37 -0800

    Init commit

---
+++ pyUI_catdb/main.py
@@ -0,0 +1,5 @@
+"""Nothing has been added to this file yet.
+   Still working on the library in the C++ data_base_project folder.
+"""
+# Print nothing lol
+print("Nothing, this is absolutely nothing lol")
