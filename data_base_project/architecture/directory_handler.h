a744cd3fc61364fe04e1b2179731e7830258ee4b
Mario Garcia <alexander.garcia18@yahoo.com>
2015-12-22 06:32:52 -0800

    Update directory_handler.h

--- data_base_project/architecture/directory_handler.h
+++ data_base_project/architecture/directory_handler.h
@@ -5,22 +5,10 @@
 #include "catdb_arch.h"
 
 #if defined(_DB_CAT_WINDOWS)
-<<<<<<< HEAD
-
- #define create_folder(filename, mode) CreateDirectory(filename, mode);
-=======
 /* macro intended to use a filename and mode. Be sure to document this. */
-#define create_folder(filename, mode) CreateDirectory(filename, mode);
->>>>>>> f7cf62d0cc198af92d9494179a6d6ab537854e1d
-
+ #define create_folder(filename, mode) CreateDirectory(filename, mode);
 #elif defined(_DB_CAT_LINUX) || defined(_DB_CAT_UNIX)
-
-<<<<<<< HEAD
  #define create_folder(filename, mode) mkdir(filename, mode);
-=======
-#define create_folder(filename, mode) mkdir(filename, mode);
->>>>>>> f7cf62d0cc198af92d9494179a6d6ab537854e1d
-
 #endif /* _DB_CAT_WINDOWS */
 
 #endif /* _DIRECTORY_HANDLER_H_ */
